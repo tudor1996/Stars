88322d45e866ab785a4b0fbbccb78101ad1ab87b
Moldovan Tudor <tmoldovan19@yahoo.com>
2021-02-27 13:20:27 +0200

    Add project files.

---
+++ C1.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int col, lin ;
+            try { 
+            
+                Console.Write("Cate stelute ");
+                col = int.Parse(Console.ReadLine());
+                Console.Write("Pe cate linii ");
+                lin = int.Parse(Console.ReadLine());
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                col = 0;
+                lin = 0;
+            }
+
+            for (int j = 0; j < lin; j++)
+            {
+                for (int i = 0; i < col; i++)
+                    Console.Write(" * ");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
